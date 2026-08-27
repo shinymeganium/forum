eb0924ac847ff4d0da9d1ba8b07f2a869e2ae727
susy <susy@email.com>
2026-08-27 10:25:40 +0300

    fixed bugs where user could edit and delete other users content

--- backend/routes/threads.js
+++ backend/routes/threads.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.route("/").
   get(async (req, res) => {
     try {
+      //await Thread.deleteMany();
       const threads = await Thread.find().sort({ "createdAt": -1 }).limit(20);
       return res.status(200).json(threads);
     }
@@ -50,16 +51,18 @@ router.route("/:id").
   }).
   put(authenticateToken, async (req, res) => {
     try {
-      const thread = await Thread.findByIdAndUpdate(
-        req.params.id,
-        { title: req.body.title, content: req.body.content },
-        { returnDocument: "after" });
+      // demonstrating authorization check before editing
+      const thread = await Thread.findById(req.params.id);
 
       if (!thread)
         return res.status(404).json({ message: "thread not found" });
 
       if (thread.author.toString() !== req.user.userId)
         return res.status(403).json({ message: "access denied" });
+
+      thread.title = req.body.title;
+      thread.content = req.body.content;
+      await thread.save();
 
       return res.status(200).json(thread);
     }
@@ -70,13 +73,16 @@ router.route("/:id").
   }).
   delete(authenticateToken, async (req, res) => {
     try {
-      const thread = await Thread.findByIdAndDelete(req.params.id);
+      // demonstrating authorization check before deleting
+      const thread = await Thread.findById(req.params.id);
 
       if (!thread)
         return res.status(404).json({ message: "thread not found" });
 
       if (thread.author.toString() !== req.user.userId)
         return res.status(403).json({ message: "access denied" });
+
+      await thread.deleteOne();
 
       return res.status(200).json({ message: "thread deleted" });
     }
