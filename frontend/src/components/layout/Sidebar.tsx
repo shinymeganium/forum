6bda151a6cbd7bd57afef42514d8fbae3cce6671
susy <3975988+shinymeganium@users.noreply.github.com>
2026-09-02 14:15:47 +0300

    sidebar links

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -1,21 +1,37 @@
+import { Link, useNavigate } from "react-router";
+import { useAuthStore } from "../../stores/authStore";
+
 export default function Sidebar() {
+  const navigate = useNavigate();
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated);
+  const logout = useAuthStore(state => state.logout);
+
   return (
-    <aside className="sticky top-16 hidden md:flex flex-col gap-3 w-45 xl:w-65 h-[calc(100vh-4rem)] p-4 border-r border-gray-400 bg-white">
-      <a href="#" className="font-medium hover:text-soft-pink">
-        Home
-      </a>
+    <aside>
+      <nav className="sticky top-16 hidden md:flex flex-col gap-3 w-45 xl:w-65 h-[calc(100vh-4rem)] p-4 border-r border-gray-400 bg-white">
+        <Link to="/" className="font-medium hover:text-soft-pink">
+          Home
+        </Link>
 
-      <a href="#" className="font-medium hover:text-soft-pink">
-        Create Thread
-      </a>
+        {isAuthenticated ?
+        (<>
+        <Link to="/create" className="font-medium hover:text-soft-pink">
+          Create a Thread
+        </Link>
+        
+        <Link to="/profile" className="font-medium hover:text-soft-pink">
+          Profile
+        </Link>
 
-      <a href="#" className="font-medium hover:text-soft-pink">
-        Profile
-      </a>
-
-      <a href="#" className="font-medium hover:text-soft-pink">
-        Logout
-      </a>
+        <button type="button" onClick={logout} className="font-medium hover:text-soft-pink text-left cursor-pointer">
+          Logout
+        </button>
+        </>) : (
+          <button type="button" onClick={() => navigate("/login")} className="font-medium hover:text-soft-pink text-left cursor-pointer">
+            Login
+          </button>
+        )}
+      </nav>
     </aside>
   );
 }
