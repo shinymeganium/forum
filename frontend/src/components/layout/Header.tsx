8d7b5025f4e8cd734d64fe3f5929ac16e705f72b
susy <susy@email.com>
2026-09-09 13:04:44 +0300

    finished mobilenav. added a link to frontpage in header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router";
 import UserMenu from "./UserMenu";
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 h-16 border-b border-gray-400 bg-white px-6 flex items-center justify-between">
-      <h1 className="text-xl font-bold text-soft-pink">
-        SoftForum
-      </h1>
+      <Link to="/">
+        <h1 className="text-xl font-bold text-soft-pink">
+          SoftForum
+        </h1>
+      </Link>
 
       <UserMenu />
     </header>
--- frontend/src/components/layout/MobileNav.tsx
+++ frontend/src/components/layout/MobileNav.tsx
@@ -1,26 +1,46 @@
-import { FaHome, FaPlus, FaUser, FaSignOutAlt } from "react-icons/fa";
+import { NavLink, useNavigate } from "react-router";
+import { useAuthStore } from "../../stores/authStore";
 import Button from "../ui/Button";
+import { FaHome, FaPlus, FaUser, FaSignOutAlt } from "react-icons/fa";
 
 function MobileNav() {
-  return (
-    <nav className="fixed bottom-0 left-0 right-0 z-50 md:hidden bg-white border-t border-gray-400 flex justify-around items-center h-18">
-      <Button>
-        <FaHome />
-      </Button>
+  const navigate = useNavigate();
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated);
+  const logout = useAuthStore(state => state.logout);
 
-      <Button variant="add">
-        <FaPlus />
-      </Button>
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    logout();
+    navigate("/");
+  };
 
-      <Button>
-        <FaUser />
-      </Button>
+  if (isAuthenticated) {
+    return (<nav className="fixed bottom-0 left-0 right-0 z-50 md:hidden bg-white border-t border-gray-400 flex justify-around items-center h-18">
+      <NavLink to="/">
+        <Button>
+          <FaHome />
+        </Button>
+      </NavLink>
 
-      <Button>
+      <NavLink to="/profile">
+        <Button>
+          <FaUser />
+        </Button>
+      </NavLink>
+      
+      <NavLink to="/create">
+        <Button variant="add">
+          <FaPlus />
+        </Button>
+      </NavLink>
+
+      <Button
+        onClick={handleLogout}>
         <FaSignOutAlt />
       </Button>
     </nav>
-  );
+    );
+  }
 }
 
 export default MobileNav;
